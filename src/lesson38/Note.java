55c906f4eef7120f43f6596d830773ec080e0f30
SemaAllahverdiyeva <sema2013allahverdiyeva@gmail.com>
2025-10-11 19:26:33 +0400

    lesson38(mən mövzunu anlamadım)

---
+++ src/lesson38/Note.java
@@ -0,0 +1,5 @@
+package lesson38;
+
+public class Note {
+//  Mən bu mövzunu yaxşı anlamadım. Videonun hardasa ilk 20-25 dəqiqəsinə baxdım, sonra bulanıqlaşdı video baxa bilmədin) PDF-də oxudum yenə çox anlamdım☹
+}
