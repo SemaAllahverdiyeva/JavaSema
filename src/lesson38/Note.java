ca91f88cd5fedced9be9326d66b32534c821d703
SemaAllahverdiyeva <sema2013allahverdiyeva@gmail.com>
2025-10-11 20:41:18 +0400

    Revert "lesson38(mən mövzunu anlamadım)"
    This reverts commit 55c906f4eef7120f43f6596d830773ec080e0f30.

--- src/lesson38/Note.java
+++
@@ -1,5 +0,0 @@
-package lesson38;
-
-public class Note {
-//  Mən bu mövzunu yaxşı anlamadım. Videonun hardasa ilk 20-25 dəqiqəsinə baxdım, sonra bulanıqlaşdı video baxa bilmədin) PDF-də oxudum yenə çox anlamdım☹
-}
